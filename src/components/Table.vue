a20d27b73daecc394ff1420052ce6cd5c6447975
andy888jam <andy888jam@gmail.com>
2022-08-03 11:16:32 +0800

    Add sorting function

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,29 +1,65 @@
 <template>
-  <table>
-    <tr>
-      <th>Misson Name</th>
-      <th>Rocket Name</th>
-      <th>Rocket Type</th>
-      <th>Launch Date</th>
-    </tr>
-    <tr v-for="launch in launches" :key="launch.mission_name">
-      <td>{{ launch.mission_name }}</td>
-      <td>{{ launch.rocket.rocket_name }}</td>
-      <td>{{ launch.rocket.rocket_type }}</td>
-      <td>{{ launch.launch_date_local }}</td>
-    </tr>
-  </table>
+  <div>
+    <table>
+      <tr>
+        <th>
+          Misson Name
+          <button @click="ascendingMission">&#9650;</button>
+          <button @click="descendingMission">&#9660;</button>
+        </th>
+        <th>
+          Rocket Name
+          <button @click="ascendingRocketName">&#9650;</button>
+          <button @click="descendingRocketName">&#9660;</button>
+        </th>
+        <th>
+          Rocket Type
+          <button @click="ascendingRocketType">&#9650;</button>
+          <button @click="descendingRocketType">&#9660;</button>
+        </th>
+        <th>
+          Launch Date
+          <button @click="ascendingLaunchDate">&#9650;</button>
+          <button @click="descendingLaunchDate">&#9660;</button>
+        </th>
+      </tr>
+      <tr v-for="launch in launches" :key="launch.mission_name">
+        <td>{{ launch.mission_name }}</td>
+        <td>{{ launch.rocket.rocket_name }}</td>
+        <td>{{ launch.rocket.rocket_type }}</td>
+        <td>{{ launch.launch_date_local | formatTime }}</td>
+      </tr>
+    </table>
+    <div class="pagination" @click="switchPage">
+      <span v-for="(item, index) in pages" :key="index">{{ item }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
+import moment from 'moment';
+
+const MOVIES_PER_PAGE = 20;
+
 export default {
   data() {
     return {
+      allLaunches: [],
+      filteredLaunches: [],
       launches: [],
+      pages: [],
+      currentPage: 1,
     };
   },
   created() {
     this.fetchLaunches();
+  },
+  filters: {
+    formatTime(dateTime) {
+      const arr = dateTime.split('T');
+      const date = arr[0];
+      return moment(date).format('YYYY/MM/DD');
+    },
   },
   methods: {
     fetchLaunches() {
@@ -47,10 +83,125 @@ export default {
       })
         .then(response => response.json())
         .then(data => {
-          this.launches = data.data.launches;
-          console.log(this.launches);
+          this.allLaunches = data.data.launches;
+          this.launches = this.getItemByPage(1);
+          this.renderPaginator(this.allLaunches.length);
         });
+    },
+    getItemByPage(page) {
+      const startIndex = (page - 1) * MOVIES_PER_PAGE;
+      return this.allLaunches.slice(startIndex, startIndex + MOVIES_PER_PAGE);
+    },
+    renderPaginator(amount) {
+      const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE);
+      const pagesArr = [];
+      for (let page = 1; page <= numberOfPages; page++) {
+        pagesArr.push(page);
+      }
+      this.pages = pagesArr;
+    },
+    switchPage(event) {
+      const currentPage = Number(event.target.innerText);
+      this.launches = this.getItemByPage(currentPage);
+    },
+    ascendingMission() {
+      
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.mission_name > b.mission_name ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    descendingMission() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.mission_name < b.mission_name ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    ascendingRocketName() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.rocket.rocket_name > b.rocket.rocket_name ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    descendingRocketName() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.rocket.rocket_name < b.rocket.rocket_name ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    ascendingRocketType() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.rocket.rocket_type > b.rocket.rocket_type ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    descendingRocketType() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.rocket.rocket_type < b.rocket.rocket_type ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    ascendingLaunchDate() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.launch_date_local > b.launch_date_local ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    descendingLaunchDate() {
+      this.allLaunches = this.allLaunches.sort(function (a, b) {
+        return a.launch_date_local < b.launch_date_local ? 1 : -1;
+      });
+      this.launches = this.getItemByPage(1);
+    },
+    filterLaunches(event) {
+      const keyword = event.target.value.trim().toLowerCase();
+      this.filteredLaunches = this.allLaunches.filter(
+        launch =>
+          launch.mission_name.toLowerCase().includes(keyword) ||
+          launch.rocket.rocket_name.toLowerCase().includes(keyword) ||
+          launch.rocket.rocket_type.toLowerCase().includes(keyword)
+      );
     },
   },
 };
 </script>
+
+<style scoped>
+table {
+  border-collapse: collapse;
+  margin: 10px auto;
+}
+table,
+th,
+td {
+  border: 1px solid black;
+  padding: 10px;
+  text-align: center;
+}
+th,
+td {
+  width: 200px;
+}
+.pagination {
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+}
+.pagination span {
+  color: black;
+  float: left;
+  padding: 8px 16px;
+  text-decoration: none;
+  transition: background-color 0.3s;
+}
+
+.pagination span.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+button {
+  all: unset;
+  margin-left: 15px;
+}
+</style>
